c301d2d494a5dd47db80ed7d7f6f81dd28b72968
Yingtao <askfsd@asdfasdfd.com>
2020-08-14 21:41:57 +0800

    home

--- python_mooc/flask_demo1.py
+++ python_mooc/flask_demo1.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
-from flask import Flask
+from flask import Flask,request
+
 app=Flask(__name__)
 
 @app.route("/user")
@@ -7,6 +8,25 @@ def hello():
 
     return "this is dengbaolei "
 
+def deng():
+    return "这不是用构造函数"
+app.add_url_rule(rule='/deng',view_func=deng)
+
+def url_name(urlname):
+    return "这是动态的url，名字是%s"%(urlname)
+
+app.add_url_rule(rule='/deng/<urlname>',view_func=url_name)
+
+@app.route("/login")
+def get_demo():
+    get_data =request.args.get('a','sdfdsf')
+    #request.args,=获取传参
+    return "获取的参数是%s%s"%(request.args,get_data)
+
+@app.route("/login_post",methods=["POST"])
+def post_demo():
+
+    return ok
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
-
