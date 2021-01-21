faa0d2da4d1f787c20be6969d72abbf1bec52cea
zhangpeng <zhangpeng@zhangpengdeMacBook-Pro.local>
2021-01-21 17:11:43 +0800

    脚本

--- python_souche/create_cars.py
+++ python_souche/create_cars.py
@@ -1,2 +1,19 @@
 # -*- coding: UTF-8 -*-
 import requests
+
+
+class Requests_methon(object):
+    def __init__(self,
+                 url, headers):
+
+        self.url = url
+        self.headers = headers
+
+    def request_get(self):
+        #requests.get(url=self.url, headers=self.headers)
+        print(self.url, self.headers)
+
+
+if __name__ == "__main__":
+    request1s = Requests_methon('22', '23123')
+    request1s.request_get()
